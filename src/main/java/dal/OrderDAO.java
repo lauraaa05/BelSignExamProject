98dd45fcdf84a54ebd67eb2ae928c2920d7aec34
Emre <aliemreuzunoglu@gmail.com>
2025-04-29 13:57:45 +0200

    We fixed maven sql jdbc dependency promlems, our pom.xml file was dead!!

--- src/main/java/dal/OrderDAO.java
+++ src/main/java/dal/OrderDAO.java
@@ -1,4 +1,28 @@
 package dal;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderDAO {
+
+    public List<String> getFormattedOrderNumbers() {
+        List<String> orders = new ArrayList<>();
+        String query = "SELECT CountryNumber, Year, Month, OrderCode FROM Orders";
+/*
+        try (Connection conn = new DBAccess().DBConnection();
+             PreparedStatement stmt = conn.prepareStatement(query);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                int countryNumber = rs.getInt("CountryNumber");
+                int year = rs.getInt("Year");
+                int month = rs.getInt("Month");
+            }
+
+        }*/
+        return null;
+    }
 }
