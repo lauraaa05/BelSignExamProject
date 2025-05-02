627d61674b5b37ccc75f79641c75a1a2e81d8c12
ermartin23 <ericalourdesmartin@gmail.com>
2025-05-02 12:28:36 +0200

    Operator manager add order number

--- src/main/java/bll/OrderManager.java
+++ src/main/java/bll/OrderManager.java
@@ -1,6 +1,7 @@
 package bll;
 import dal.OrderDAO;
 
+import java.io.File;
 import java.util.List;
 
 public class OrderManager {
@@ -9,5 +10,9 @@ public class OrderManager {
 
     public List<String> getOrderNumbersAsList() {
         return od.getFormattedOrderNumbers();
+    }
+
+    public List<File> getOrderImages(String orderNumber) {
+        return List.of();
     }
 }
