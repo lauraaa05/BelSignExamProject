f8408dd566c8038056dc03fdf202b3223ac2f12d
katja981 <katstr01@easv365.dk>
2025-05-06 14:06:59 +0200

    Added username to the login be

--- src/main/java/be/Login.java
+++ src/main/java/be/Login.java
@@ -4,12 +4,14 @@ class Login {
 
     private int operatorId;
     private String role;
+    private String username;
     private String password;
 
 
-    public Login(int operatorId, String role, String password) {
+    public Login(int operatorId, String role, String username, String password) {
         this.operatorId = operatorId;
         this.role = role;
+        this.username = username;
         this.password = password;
     }
 
@@ -29,6 +31,14 @@ class Login {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
