87c5fa4ff9b76747674fb4f7aea0f4bd8e465712
Emre <aliemreuzunoglu@gmail.com>
2025-05-06 13:11:01 +0200

    I have a basic design for all orders menu

---
+++ src/main/java/dk/easv/belsignexamproject/TestQCUAllOrders.java
@@ -0,0 +1,24 @@
+package dk.easv.belsignexamproject;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TestQCUAllOrders extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(OperatorLogInApp.class.getResource("/view/QCUAllOrders.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Operator Log In");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
