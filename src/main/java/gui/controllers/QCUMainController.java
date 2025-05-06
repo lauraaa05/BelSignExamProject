5797eb61a8a18c889993afafd56bd3bf9eea3549
Emre <aliemreuzunoglu@gmail.com>
2025-05-06 14:12:56 +0200

    I polished the QCUReport and a go back button which take the user to mainQCu scene   it has a cool design now

--- src/main/java/gui/controllers/QCUMainController.java
+++ src/main/java/gui/controllers/QCUMainController.java
@@ -5,11 +5,18 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import utilities.SceneNavigator;
 
 import java.io.IOException;
 
 public class QCUMainController {
+
+    @FXML
+    private Button btnOpenReport;
+
+    private final SceneNavigator sceneNavigator = new SceneNavigator();
 
     @FXML
     private void handleSignOutButtonClick(ActionEvent event) {
@@ -23,6 +30,14 @@ public class QCUMainController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    private void btnOpenReportAction(ActionEvent actionEvent) {
+        sceneNavigator.switchTo(actionEvent,"QCUReport.fxml");
+    }
+
+    private void sceneNavigator(String s) {
     }
 }
 
