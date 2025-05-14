44f817255367422462766bb69ab21c33f70ecba9
Emre <aliemreuzunoglu@gmail.com>
2025-05-14 20:25:10 +0200

    Fixed some little bugs

--- src/main/java/be/Picture.java
+++ src/main/java/be/Picture.java
@@ -35,6 +35,22 @@ public class Picture {
         this.side = ""; // Optional fallback if you don't know side here
     }
 
+    public Picture(byte[] imageBytes, String fileName, LocalDateTime timestamp, String orderNumber, String side) {
+        this.image = imageBytes;
+        this.fileName = fileName;
+        this.timestamp = timestamp;
+        this.orderNumber = orderNumber;
+        this.side = side;
+    }
+
+    public Picture(byte[] image, LocalDateTime timestamp, String side) {
+        this.image = image;
+        this.timestamp = timestamp;
+        this.side = side;
+        this.fileName = "";
+        this.orderNumber = "";
+    }
+
     public int getImageId() {
         return imageId;
     }
@@ -77,5 +93,9 @@ public class Picture {
 
     public void setOrderNumber(String orderNumber) {
         this.orderNumber = orderNumber;
+    }
+
+    public void setSide(String side) {
+        this.side = side;
     }
 }
