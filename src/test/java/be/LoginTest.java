1dd6c86283b5ec7902349245adfad4f982455436
katja981 <katstr01@easv365.dk>
2025-06-01 13:24:47 +0200

    Added test class

--- src/test/java/be/LoginTest.java
+++ src/test/java/be/LoginTest.java
@@ -1,4 +1,32 @@
 package be;
 
-public class LoginTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginTest {
+
+    @Test
+    void testConstructorAndGetters() {
+        Login login = new Login(1, "admin", "user1", "pass123");
+
+        assertEquals(1, login.getOperatorId());
+        assertEquals("admin", login.getRole());
+        assertEquals("user1", login.getUsername());
+        assertEquals("pass123", login.getPassword());
+    }
+
+    @Test
+    void testSetters() {
+        Login login = new Login(0, "", "", "");
+
+        login.setOperatorId(2);
+        login.setRole("user");
+        login.setUsername("user2");
+        login.setPassword("secure456");
+
+        assertEquals(2, login.getOperatorId());
+        assertEquals("user", login.getRole());
+        assertEquals("user2", login.getUsername());
+        assertEquals("secure456", login.getPassword());
+    }
 }
